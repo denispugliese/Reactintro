67b3ddb2229c2c321f7c84b110b007e7429ee598
Denis Pugliese <denis.pugliese1@gmail.com>
2022-11-15 23:01:16 -0500

    reactintro

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        React is awesome!
         </p>
+        <h1>Testing</h1>
+        <p>Denis Pugliese</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I WILL learn React
         </a>
       </header>
     </div>
