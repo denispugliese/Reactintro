5f6e676c2619cd30161032dbfc3fe658b633260d
Denis Pugliese <denis.pugliese1@gmail.com>
2022-12-02 15:13:18 -0500

    router hooks exercise

--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -1,48 +1,76 @@
 import { useState, useEffect } from "react";
-import { filterFilmsByDirector, getListOf } from "../helpers/film.helpers"
-
+import {
+  filterFilmsByDirector,
+  getListOf,
+  getFilmStats,
+} from "../helpers/film.helpers";
+import { Link } from "react-router-dom";
 
 function FilmsPage(props) {
-    let [list, setList] = useState([]);
-    let [searchDirector, setSearchDirector] = useState("");
+  let [list, setList] = useState([]);
+  let [searchDirector, setSearchDirector] = useState("");
 
-    function getFilms() {
-        fetch("https://ghibliapi.herokuapp.com/films")
-        .then((response) => response.json())
-        .then((films) => setList(films))
-        .catch((error) => console.error(error));
-    }
+  function getFilms() {
+    fetch(`https://ghibliapi.herokuapp.com/films`)
+      .then((response) => response.json())
+      .then((films) => setList(films))
+      .catch((error) => console.error(error));
+  }
 
-    useEffect(() => {
-        getFilms();
-    }, []);
+  useEffect(() => {
+    getFilms();
+  }, []);
 
-    let filmsByDirector = filterFilmsByDirector(list, searchDirector);
-    let directors = getListOf(list, "director");
+  let filmsByDirector = filterFilmsByDirector(list, searchDirector);
+  let directors = getListOf(list, "director");
+  let { avg_score, latest, total } = getFilmStats(filmsByDirector);
 
-        return (
+  return (
+    <div>
+      <h1>Studio Ghibli Films</h1>
+      <form>
+        <label htmlFor="searchDirector">Filter by Director</label>
+        <select
+          name="searchDirector"
+          id="searchDirector"
+          value={searchDirector}
+          onChange={(e) => setSearchDirector(e.target.value)}
+        >
+          <option value="">All</option>
+          {directors.map((director, idx) => {
+            return (
+              <option key={director + idx} value={director}>
+                {director}
+              </option>
+            );
+          })}
+        </select>
+      </form>
+      <div>
         <div>
-            <h1>Studio Ghibli Films</h1>
-            <form>
-                <label htmlFor="searchDirector">Filter by Director</label>
-                <select name="searchDirector" id="searchDirector" value={searchDirector} 
-                onChange={(e) => setSearchDirector(e.target.value)}
-                >
-                    <option value="">All</option>
-                    {directors.map((director, idx) => {
-                        return (
-                        <option key={director + idx} value={director}>{director}</option>
-                      );
-                    })}
-                </select>
-            </form>
-            <ul>
-            {filmsByDirector.map((film) => {
-              return <li key={film.id}>{film.title}</li>
-            })}
-           </ul>
+          <span># Of Films</span>
+          <span>{total}</span>
         </div>
-    );
+        <div>
+          <span>Average Rating</span>
+          <span>{avg_score.toFixed(2)}</span>
+        </div>
+        <div>
+          <span>Latest Film</span>
+          <span>{latest}</span>
+        </div>
+      </div>
+      <ul>
+        {filmsByDirector.map((film) => {
+          return (
+            <li key={film.id}>
+              <Link to={`/films/${film.id}`}>{film.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default FilmsPage;
